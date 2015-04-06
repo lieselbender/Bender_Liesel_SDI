3e16c38e597667b7f8ac0d0a96be8488902843eb
Liesel Bender <liesel@fullsail.edu>
2015-04-06 11:35:08 -0400

    Prompted user for name
    Prompted user for name

--- setup_github/js/script.js
+++ setup_github/js/script.js
@@ -2,4 +2,9 @@
  * Created by lieselbender on 4/6/15.
  */
 
-alert("testing 1, 2, 3!");
+//alert("testing 1, 2, 3!");
+
+console.log("testing out console.log");
+
+var userInput =  prompt("please type in your name:");
+console.log(userInput);
